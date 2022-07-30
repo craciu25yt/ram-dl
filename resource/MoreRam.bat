f4e01521f139c37d83399826364b6bab47e2e0cd
craciu25yt <48807342+craciu25yt@users.noreply.github.com>
2022-07-31 00:41:44 +1000

    Create MoreRam.bat

---
+++ resource/MoreRam.bat
@@ -0,0 +1,36 @@
+@echo off
+Title Downloading ram!
+echo Downloading.
+timeout /t 1 /nobreak >nul
+cls
+echo Downloading..
+timeout /t 1 /nobreak >nul
+cls
+echo Downloading...
+timeout /t 1 /nobreak >nul
+cls
+echo Downloading.
+timeout /t 1 /nobreak >nul
+cls
+echo Downloading..
+timeout /t 1 /nobreak >nul
+cls
+echo Downloading...
+timeout /t 1 /nobreak >nul
+cls
+echo Downloaded!
+echo.
+
+::ik u can with any :skull:
+echo Press any SPACE to install!
+pause>nul
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+cls
+echo My friend, you can't download ram :c. 
+echo All that was trick to RickRoll people and
+echo warn about that. Idk if you just run that
+echo batch file, but you need to take care about
+echo that. You can learn more about batch in YouTube ;D
+echo.
+echo Press "X" to leave. (If you press another key you will listen a weird sound)
+choice /c x>nul
